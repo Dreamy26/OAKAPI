707108a5872c68b5aed877372928a2d15062ac19
Dreamy247 <wilson.aj19@gmail.com>
2022-10-11 23:32:05 -0400

    build: Adds properties to Person & Position

--- OAKAPI/Model/Position.cs
+++ OAKAPI/Model/Position.cs
@@ -1,6 +1,19 @@
-﻿namespace OAKAPI.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OAKAPI.Model
 {
     public class Position
     {
+        public Position()
+        {
+            Persons = new HashSet<Person>();
+        }
+        [Key]
+        public int PositionId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public virtual ICollection<Person> Person { get; set; }
+
+
     }
 }
